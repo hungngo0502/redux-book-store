999c2c9b2e490fcbf9fa5836657fc49eb0545ae5
tan <tan.nguyennhut@gotadi.com>
2022-11-09 21:29:24 +0700

    src

--- src/theme/MThemeProvider.js
+++ src/theme/MThemeProvider.js
@@ -1,43 +1,39 @@
 import { CssBaseline } from "@mui/material";
-import {
-    createTheme,
-    ThemeProvider,
-} from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const PRIMARY = {
-    lighter: "#0b8c12",
-    light: "#0a8211",
-    main: "#08660D",
-    dark: "#064a09",
-    darker: "#042d06",
-    contrastText: "#FFF",
+  lighter: "#dfff00",
+  light: "#ffaf00",
+  main: "#ff5f00",
+  dark: "#ffffff",
+  darker: "#ff6f23",
+  contrastText: "#0b8c12",
 };
 const SECONDARY = {
-    lighter: "#FFD07F",
-    light: "#FDA65D",
-    main: "#FF8243",
-    dark: "#E26A2C",
-    darker: "#cc571f",
-    contrastText: "#FFF",
+  lighter: "#FFD07F",
+  light: "#FF9999",
+  main: "#FF8243",
+  dark: "#E26A2C",
+  darker: "#cc571f",
+  contrastText: "#FFD07F",
 };
 
-
 function MThemeProvider({ children }) {
-    const themeOptions = {
-        palette: {
-            primary: PRIMARY,
-            secondary: SECONDARY,
-        }
-    };
+  const themeOptions = {
+    palette: {
+      primary: PRIMARY,
+      secondary: SECONDARY,
+    },
+  };
 
-    const theme = createTheme(themeOptions);
+  const theme = createTheme(themeOptions);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {children}
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 }
 
 export default MThemeProvider;
